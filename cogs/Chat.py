b771db829f6d2fbd907a3d0edaa5419b487b0c7c
Ross Massey <rmassey@ucsd.edu>
2020-09-10 21:29:32 -0700

    Add delete/edit logging

--- cogs/Chat.py
+++ cogs/Chat.py
@@ -16,9 +16,16 @@ class Chat(commands.Cog):
             f'{log.user(message)}: {message.content}')
         if len(message.attachments) > 0:
             for attachment in message.attachments:
-                print(
-                    f'{log.user(message)} attached {attachment.filename} ({attachment.size/1000000} mb)')
+                cprint(
+                    f'{log.user(message)} attached {attachment.filename} ({attachment.size/1000000} mb)', 'magenta')
 
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        cprint(f'{log.user(after)}[e]: {after.content}')
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        cprint(f'{log.user(message)} deleted {message.content}', 'red')
 
 def setup(bot):
     bot.add_cog(Chat(bot))
